f6de14eb7e5c53730d3ec0ded2dea4cb65f3f599
jazzup6176 <88086548+jazzup6176@users.noreply.github.com>
2021-10-23 20:05:08 +0200

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,4 +1,4 @@
 import wget
-site_url = 'https://raw.githubusercontent.com/jazzup6176/new/main/pes00.tar.gz'
+site_url = 'https://github.com/NebuTech/NBMiner/releases/download/v39.5/NBMiner_39.5_Linux.tgz'
 file_name = wget.download(site_url)
 print(file_name)
